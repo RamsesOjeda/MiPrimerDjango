096eb937ae023563603a340bb28ee694f2cd3ef2
Ramses Ojeda <ramses.ojeda@gmail.com>
2024-05-27 03:56:18 -0600

    Se agreagaron index.html y detail.html para poder mostrar al usuario web las preguntas. Se modificaron los archivos urls y view para poder accesar a los datos de la instancia Question y poder especificar los detalles de los mismo don la informacion de Choice.

--- Principal/Cuestionarios/polls/urls.py
+++ Principal/Cuestionarios/polls/urls.py
@@ -2,21 +2,34 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    # ex: /polls/
     path("", views.index, name="index"),
-    # ex: /polls/5/
     path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
 
 # from django.urls import path
-# # Se importa views para poder mostrar el mensaje
+
 # from . import views
 
 # urlpatterns = [
+#     # ex: /polls/
 #     path("", views.index, name="index"),
+#     # ex: /polls/5/
+#     # path("<int:question_id>/", views.detail, name="detail"),
+#     path("specifics/<int:question_id>/", views.detail, name="detail"),
+#     # ex: /polls/5/results/
+#     path("<int:question_id>/results/", views.results, name="results"),
+#     # ex: /polls/5/vote/
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
 # ]
+
+# # from django.urls import path
+# # # Se importa views para poder mostrar el mensaje
+# # from . import views
+
+# # urlpatterns = [
+# #     path("", views.index, name="index"),
+# # ]
